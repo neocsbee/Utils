0a276267368cad85c95d170e8014fcd2107784b7
Jeyanthi S N <jeyraj_ns@yahoo.com>
2013-03-19 11:02:50 -0700

    Update tannbc.cc
    Added copyright information and license taken from gnu.org

--- tannbc.cc
+++ tannbc.cc
@@ -2,8 +2,22 @@
  * =====================================================================================
  *
  *       Filename:  tannbc.cc
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/license
+    s/>.
  *
       Tree augmented naive bayes classifier
+      
  *    Description:  Input to the program is the data matrix with the class information 
  *    in the first column and the rest of columns has the feature values. One line of the 
  *    inputfile looks like below:
@@ -27,8 +41,8 @@
  *       Compiler:  gcc
  *
  *         Author:  Jeyanthi S N (jey), jsalemna@eecs.wsu.edu
- *        Company:  Washington State University, Pullman
- *
+ *         Company:  Washington State University, Pullman
+ *    Copyright 2011 - 2013, Jeyanthi S N
  * =====================================================================================
  */
 #include <iostream>
